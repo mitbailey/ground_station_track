33d56b2bfa5fc5d6747302e618dbd9b09c73737f
Mit Bailey <mitbailey99@gmail.com>
2021-10-14 15:01:14 -0400

    Adjusted parking Elevation radians.

--- src/track.cpp
+++ src/track.cpp
@@ -161,7 +161,7 @@ void *tracking_thread(void *args)
             obscured_azel = dish->GetLookAngle(target->FindPosition(DateTime::Now(true)));
             // ideal.azimuth = 1.5708; // 90deg, this is in radians
             // Change elevation to 90' when parked.
-            ideal.elevation = 1.5708; // 90deg, this is in radians.
+            ideal.elevation = M_PI / 2; // 90deg, this is in radians.
             /// D O   N O T   C H A N G E   A Z   W H E N   P A R K E D
             dbprintlf(YELLOW_FG "PARKING AT AZ:EL %.2f:%.2f (%.2f:%.2f)", ideal.azimuth DEG, ideal.elevation DEG, obscured_azel.azimuth DEG, obscured_azel.elevation DEG);
         }
